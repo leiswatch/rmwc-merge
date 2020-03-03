68b01e1bedfc9a829864568200e9528d9406ede7
Michał Kowalewski <michal.kowalewski@uxpin.com>
2020-03-03 13:04:59 +0100

    Change default text button

--- src/Button/presets/0-default.jsx
+++ src/Button/presets/0-default.jsx
@@ -3,6 +3,6 @@ import Button from "../Button";
 
 export default (
   <Button uxpId="1" ripple raised style={{ width: "100%" }}>
-    Click me because this is test
+    Click me because this is release 2.3
   </Button>
 );
